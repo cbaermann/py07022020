9aaba38c0d7db902d6bd04fff97f94c05abe0eb3
Cody Baermann <cbaermann@comcast.net>
2020-07-02 14:11:03 -0700

    nasa api

--- NASA_api.py
+++ NASA_api.py
@@ -1,7 +1,32 @@
 import requests 
-# get_api_request = requests.get("https://api.nasa.gov/planetary/apod?api_key=MiXU3cFZ63yO9b4xcRiLEYsgBbl6nxhuwSawKcRC")
-get_api_request = requests.get("https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY#")
 
-response_data = get_api_request.json()
+with open("../myapi.key", "r") as secretkey:
+    myapikey = secretkey.read()
 
-print(response_data["near_earth_objects"]["name"])
+def main():
+    search_date_input = input("What date are you searching for? YYYY-MM-DD Format only.\n")
+    
+    asteroid_url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date=" \
+        f"{search_date_input}&end_date={search_date_input}&api_key={myapikey}" 
+    
+    mars_url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" \
+            f"{search_date_input}&api_key={myapikey}"
+    
+    asteroid_api_request = requests.get(asteroid_url)
+    mars_api_request = requests.get(mars_url)
+
+    if asteroid_api_request.status_code != 200:
+        sys.exit("API Status Code Error, check API Provider.")
+
+    asteroid_api_data = asteroid_api_request.json()
+    mars_api_data = mars_api_request.json()
+
+    number_of_asteroids = asteroid_api_data['element_count']
+    mars_photo_json_pull = mars_api_data['photos']
+    mars_photo = mars_photo_json_pull[0]["img_src"]
+
+    print(f"There were {number_of_asteroids} asteroids monitored on this date by NASA.")
+    print(f"Image of Mars URL: {mars_photo}")
+
+
+main()
